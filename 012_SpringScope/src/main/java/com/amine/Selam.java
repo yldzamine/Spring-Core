b4af805d1431bccb13b3d216129c1574f14caf1c
yldzamine <yildizamine95@gmail.com>
2018-12-28 20:57:17 +0200

    Spring Core-12

---
+++ 012_SpringScope/src/main/java/com/amine/Selam.java
@@ -0,0 +1,18 @@
+package com.amine;
+
+public class Selam {
+
+	private String mesaj;
+	//-----------------------
+	public String getMesaj() {
+		return mesaj;
+	}
+
+	public void setMesaj(String mesaj) {
+		this.mesaj = mesaj;
+	}
+	//-----------------------
+	public void goster() {
+		System.out.println("BENDEN SELAM OLSUN BOLU BEYİNE");
+	}
+}
