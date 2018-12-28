49d7ca24c5c8f9750b4b202eeb1547134f457caf
yldzamine <yildizamine95@gmail.com>
2018-12-28 21:44:06 +0200

    Spring Core-45

---
+++ Spring_SpEL/src/main/java/com/amine/Kitap.java
@@ -0,0 +1,23 @@
+package com.amine;
+
+public class Kitap {
+ private String eseradi;
+ private String eserISBN;
+//----------------------------------
+public String getEserISBN() {
+	return eserISBN;
+}
+public void setEserISBN(String eserISBN) {
+	this.eserISBN = eserISBN;
+}
+//------------------------------- 
+public String getEseradi() {
+	return eseradi;
+}
+public void setEseradi(String eseradi) {
+	this.eseradi = eseradi;
+}
+//------------------------------- 
+
+//------------------------------- 
+}
---
+++ Spring_SpEL/src/main/java/com/amine/Yazar.java
@@ -0,0 +1,43 @@
+package com.amine;
+
+public class Yazar {
+
+	private Kitap kitap;
+	private String adi,soyadi;
+	private String kitapadi;
+	//------------------------------- 	
+	public Kitap getKitap() {
+		return kitap;
+	}
+	public void setKitap(Kitap kitap) {
+		this.kitap = kitap;
+	}
+	//------------------------------- 
+	public String getAdi() {
+		return adi;
+	}
+	public void setAdi(String adi) {
+		this.adi = adi;
+	}
+	//------------------------------- 
+	public String getSoyadi() {
+		return soyadi;
+	}
+	public void setSoyadi(String soyadi) {
+		this.soyadi = soyadi;
+	}
+	//------------------------------- 
+	public String getKitapadi() {
+		return kitapadi;
+	}
+	public void setKitapadi(String kitapadi) {
+		this.kitapadi = kitapadi;
+	}
+	//------------------------------- 
+	@Override
+	public String toString() {
+		return "Yazar [kitap=" + getKitap().getEseradi() +
+				", \nadi=" + getAdi() + ", \nsoyadi=" + getSoyadi() + ", \nkitapadi=" + getKitapadi() + "]";
+	}
+	
+}
